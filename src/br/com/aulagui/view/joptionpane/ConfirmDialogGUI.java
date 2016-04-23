7a8a7e5ee8cdf726d32c8629722c66e796913fae
ricardogeraldi <ricardogeraldi@gmail.com>
2016-04-23 12:46:45 -0300

    Alteração ConfirmDialogGUI

--- src/br/com/aulagui/view/joptionpane/ConfirmDialogGUI.java
+++ src/br/com/aulagui/view/joptionpane/ConfirmDialogGUI.java
@@ -10,25 +10,23 @@ import javax.swing.JOptionPane;
 public class ConfirmDialogGUI {
     private static Component janelaConfirmacao;
     
-    // Define a classe como principal
+    // Define a classe como Principal
     public static void main(String[] args) {
         
-        // Mensagens de confirmação (optionType)
+        // Mensagens de Confirmação (OptionType)
         JOptionPane.showConfirmDialog(janelaConfirmacao, 
-                                      "Prossegue com a tarefa?", 
-                                      "Escolha uma opção",
+                                      "Prossegue com a Tarefa?", 
+                                      "Escolha uma Opção",
                                       JOptionPane.YES_NO_OPTION);
         
         JOptionPane.showConfirmDialog(janelaConfirmacao, 
-                                      "Deseja realizar esta ação?", 
-                                      "Escolha uma opção",
+                                      "Deseja realizar esta Ação?", 
+                                      "Escolha uma Opção",
                                       JOptionPane.YES_NO_CANCEL_OPTION);
         
         JOptionPane.showConfirmDialog(janelaConfirmacao, 
                                       "Tem Certeza?", 
-                                      "Escolha uma opção",
-                                      JOptionPane.OK_CANCEL_OPTION);
-        
-    }
-    
+                                      "Escolha uma Opção",
+                                      JOptionPane.OK_CANCEL_OPTION); 
+    }  
 }
